7ca9a67f15933b13427d7f95df613ce206ad4dd8
Deeraj N <mail4deeraj@gmail.com>
2026-02-11 21:05:08 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://doctor-patient-translator-ulpe.onrender.com";
 
 export const createSession = async () => {
   const res = await axios.post(`${API_BASE}/sessions/`);
